4162ae17707b35126ede2ca1867c5ea3c7f7ed68
P81000 <p.masteguin@unifesp.br>
2023-12-13 15:52:48 -0300

    [FEATURE] Packer config for papercolor-theme

--- lua/masteguin/packer.lua
+++ lua/masteguin/packer.lua
@@ -40,4 +40,5 @@ return require('packer').startup(function(use)
     use({
         'lervag/vimtex', 
     })
+    use( 'NLKNguyen/papercolor-theme' )
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -129,6 +129,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\maste\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
+  ["papercolor-theme"] = {
+    loaded = true,
+    path = "C:\\Users\\maste\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\papercolor-theme",
+    url = "https://github.com/NLKNguyen/papercolor-theme"
+  },
   ["plenary.nvim"] = {
     loaded = true,
     path = "C:\\Users\\maste\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
