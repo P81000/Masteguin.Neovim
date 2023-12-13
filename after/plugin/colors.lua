c8ed854d673fa8e840bae3af4ad2c2847326a560
P81000 <p.masteguin@unifesp.br>
2023-12-13 16:21:47 -0300

    [FEAT] Change between installed themes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,4 @@
-function ColorMyPencils(color)
+function ColorRose(color)
 	color = color or 'rose-pine'
 	vim.cmd.colorscheme(color)
 
@@ -6,4 +6,20 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
-ColorMyPencils()
+function ColorPaper(color)
+    color = color or 'papercolor'
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
+function ColorDefault(color)
+    color = 'rose-pine'
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
+ColorDefault()
