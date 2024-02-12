a1dfabd0ef33f1622785831cba4192c9e9b9efe0
P81000 <p.masteguin@unifesp.br>
2024-02-12 17:59:37 -0300

    [FIX] return if not in a markdown file

--- after/plugin/glow.lua
+++ after/plugin/glow.lua
@@ -6,8 +6,8 @@ end
 local function toggle_glow()
     if not ismd() and not en then
         print("Error: Glow can only be used on Markdown files.")
+        return
     end
-
     vim.cmd("Glow" .. (en and "!" or ""))
     en = not en
 end
