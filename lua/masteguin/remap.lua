9e2d655c17b005eeb21a9f5c594a4fd94d6004d3
P81000 <p.masteguin@unifesp.br>
2024-02-12 11:58:25 -0300

    [FEAT] Handle if trying to quit with unsaved changes

--- lua/masteguin/remap.lua
+++ lua/masteguin/remap.lua
@@ -12,6 +12,17 @@ vim.api.nvim_set_keymap("i", "'", '"', { noremap = true })
 vim.api.nvim_set_keymap("i", '"', "'", { noremap = true })
 
 vim.keymap.set("n", "<leader>w", function() vim.cmd("w") end, { noremap = true, silent=true })
-vim.keymap.set("n", "<leader>q", function() vim.cmd("q") end, { noremap = true, silent=true })
+vim.keymap.set("n", "<leader>q", function()
+    local modifiedBuffers = vim.tbl_filter(function(buf)
+        return vim.api.nvim_buf_get_option(buf, "modified")
+    end, vim.api.nvim_list_bufs())
+
+    if #modifiedBuffers > 0 then
+        vim.fn.input("Error: There are unsaved changes. Save or discard them before quiting! Press ENTER or type command to continue")
+	    vim.cmd("buffer " .. modifiedBuffers[1])
+    else
+        vim.cmd("q")
+    end
+end, { noremap = true, silent=true })
 vim.keymap.set("n", "<leader>wq", function() vim.cmd("wq") end, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>so", function() vim.cmd("so") end, { noremap = true, silent = true })
