bee3de2528cc1090419adc763124abb517bbb304
P81000 <p.masteguin@unifesp.br>
2025-04-19 22:42:41 -0300

    Updates on telescope and new remap to comment/uncomment C files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,11 +4,16 @@ local utils = require('telescope.utils')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
+vim.keymap.set('n', '<leader>pg', function()
+    local search_query = vim.fn.input("Grep > ")
+    builtin.grep_string({ search = search_query })
+end)
+
 vim.keymap.set('n', '<leader>ps', function()
     local search_query = vim.fn.input("Grep > ")
 	builtin.grep_string({
         search = search_query,
         cwd = utils.buffer_dir(),
         only_sort_text = true
-    });
+    })
 end)
--- lua/masteguin/remap.lua
+++ lua/masteguin/remap.lua
@@ -37,3 +37,28 @@ vim.keymap.set("v", "<leader>0", "$", { noremap = true })
 
 vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>tp", ":tabprev<CR>", { noremap = true })
+
+-- Grupo para arquivos C/C++/header
+vim.api.nvim_create_augroup("CommentCFiles", { clear = true })
+
+-- Mapeamento global: mostra aviso se não for .c, .cpp ou .h
+vim.keymap.set("v", "<leader>c", function()
+  print("Only available in C, C++, or header files.")
+end, { noremap = true })
+
+vim.keymap.set("v", "<leader>s", function()
+  print("Only available in C, C++, or header files.")
+end, { noremap = true })
+
+-- Atalhos específicos para arquivos C/C++/header
+vim.api.nvim_create_autocmd("FileType", {
+  group = "CommentCFiles",
+  pattern = { "c", "cpp", "h" },
+  callback = function()
+    -- Comentar: adiciona // no início das linhas selecionadas
+    vim.keymap.set("v", "<leader>c", [[:s/^/\/\/ /<CR>]], { buffer = true, noremap = true })
+
+    -- Descomentar: remove // e espaço se estiverem no início da linha
+    vim.keymap.set("v", "<leader>s", [[:s/^\/\/\s\?//<CR>]], { buffer = true, noremap = true })
+  end,
+})
